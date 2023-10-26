e98a1713b40158ff5459a1d01731eb824a5007b3
viit0r <vituuoliveira1@gmail.com>
2023-10-25 22:22:53 -0300

    Criação do projeto

---
+++ src/main/java/com/viit0r/consultacepapi/ConsultaCepApiApplication.java
@@ -0,0 +1,13 @@
+package com.viit0r.consultacepapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCepApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viit0r/consultacepapi/ConsultaCepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viit0r.consultacepapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
